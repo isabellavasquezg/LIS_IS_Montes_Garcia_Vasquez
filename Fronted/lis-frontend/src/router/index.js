7ff8aac49384dbdf85371492088d6dd52ff50d1a
KevinGJ23 <kevin.garciaj@ude.edu.com>
2025-10-31 16:23:40 -0500

    frontend-login

---
+++ Fronted/lis-frontend/src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+const router=createRouter({
+    history:createWebHistory(),
+    routes:[
+        {
+            path:'/',
+            name:'Home',
+            component:()=>import('../views/LoginView.vue')
+        }
+    ]
+});
+export default router;
---
+++ Fronted/lis-frontend/src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <div class="background-login-container"> 
+    <div class="bacground-login-color">
+
+    </div>
+  </div>
+</template>
+
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+    }
+    .background-login-container {
+        margin: 0;
+        padding: 0;
+        background-image: url(../../public/images/Background.jpg);
+        background-size: cover;
+        height: 100vh;
+        display: flex;
+    }
+    .bacground-login-color {
+        background-color:#00506c;
+        width: 100%;
+        height: 100vh;
+        opacity: 0.9;
+        margin: 0;
+        padding: 0;
+    }
+</style>
