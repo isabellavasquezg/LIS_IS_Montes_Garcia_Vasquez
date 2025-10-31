6176aaeb67d161d74823e08f01743c020d066650
KevinGJ23 <kevin.garciaj@ude.edu.com>
2025-10-31 17:36:17 -0500

    organizar_login

--- Fronted/lis-frontend/src/views/LoginView.vue
+++ Fronted/lis-frontend/src/views/LoginView.vue
@@ -4,7 +4,24 @@
 <template>
   <div class="background-login-container"> 
     <div class="bacground-login-color">
-
+        <div class="login-left">
+            <div class="logo-container">
+                <div class="logo-image">
+                </div>
+            </div>
+        </div>
+        <div class="login-right">
+            <div class="formulario-container">
+                <form>
+                    <div class="inputs-login">
+                        <input type="text" placeholder="User Name"/> 
+                    </div>
+                    <div class="inputs-login">
+                        <input type="password" placeholder="Password"/> 
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -29,5 +46,44 @@
         opacity: 0.9;
         margin: 0;
         padding: 0;
+        display: flex;
+        flex-direction: row;
+    }
+    .login-left, .login-right {
+        height: 100vh;
+        margin:0; 
+        display: flex; 
+        align-items: center;  
+    }
+    .login-right{
+        width: 52%;
+        justify-content: left; 
+    }
+    .login-left{
+        width: 48%;
+        padding: 100px,0,100px,0;
+    }
+    .logo-container {
+        width: 100vh;
+        height: 40vh;
+        border-right: 3px solid white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .logo-image{
+        width: 50vh;
+        height: 100%;
+        background-image: url(../../public/images/logo_login.png);
+    }
+    .formulario-container {
+        width: 50vh;
+        height: 40vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .inputs-login {
+        margin: 20px;
     }
 </style>
