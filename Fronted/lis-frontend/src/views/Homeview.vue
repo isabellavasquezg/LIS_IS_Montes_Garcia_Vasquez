e9f0bddc552bd27b067f28cf9cbffc9513d326b3
KevinGJ23 <kevin.garciaj@ude.edu.com>
2025-11-02 23:51:24 -0500

    carpetas_Modulos

--- Fronted/lis-frontend/src/views/Homeview.vue
+++ Fronted/lis-frontend/src/views/Homeview.vue
@@ -1,7 +1,10 @@
 <script>
 </script>
 <template>
-
+    <div class="homeview-container">
+        <h1>Bienvenido a la página principal</h1>
+        <p>Esta es la vista de inicio después de iniciar sesión.</p>
+    </div>
 </template>
 <style>
 </style>
